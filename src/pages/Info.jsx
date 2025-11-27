dc46170034fac63519129bf9d2f6914cab4049b9
hymalaya12 <hymalaya12@naver.com>
2025-11-27 14:24:44 +0900

    재료추가화면

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -10,12 +10,31 @@ const Info = () => {
   const history = useNavigate();
 
   // TODO: set함수 추가하기
-  const [ingredientList] = useState([]); // 사용자가 입력할 재료 목록
+  const [ingredientList, setIngredientList] = useState([]); // 사용자가 입력할 재료 목록
 
   const addIngredient = () => {
-    console.log("재료 추가하기");
+    //기본 데이터 추가
+    const id = Date.now();
+
+    const newItem = {
+      id,
+      label: `ingredient_${id}`,
+      text: "재료명",
+      value: "",
+    };
+
+    //기존 배열에 객체 추가
+    setIngredientList([...ingredientList, newItem]);
+    //console.log("재료 추가하기");
   };
 
+  const handleRemove = (selectedId) => {
+    console.log("🚀 ~ handleRemove ~ selectedId:", selectedId);
+    const filterIngredientList = ingredientList.filter(
+      (item) => item.id !== selectedId
+    );
+    setIngredientList(filterIngredientList);
+  };
   const handleNext = () => {
     //console.log("chat페이지로 이동");
     // 미션 : chat페이지로 이동 구현
@@ -44,7 +63,11 @@ const Info = () => {
             {/* START:input 영역 */}
             <div>
               {ingredientList.map((item) => (
-                <InfoInput key={item.id} content={item} />
+                <InfoInput
+                  key={item.id}
+                  content={item}
+                  onRemove={handleRemove}
+                />
               ))}
             </div>
             {/* END:input 영역 */}
